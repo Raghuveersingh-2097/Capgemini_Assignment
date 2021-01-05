705dcc55040a4387dfebdb85d026959e4f2ddfa8
Raghav Singh <raghuvversingh2097@gmail.com>
2021-01-05 16:21:24 +0530

    First Commit

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+class Fibonacci{
+public static void main(String args[]){
+int first_value=0;
+int second_Value=1;
+int next_Element_Helper=0;
+System.out.print(first_value+" "+b);
+for(int i=0;i<8;i++) {
+   next_Element_Helper=first_value+second_Value;
+   first_value=second_Value;
+   second_Value=next_Element_Helper;
+System.out.print(" "+next_Element_Helper);
+}
+}
+}
---
+++ RupeesToDollar.java
@@ -0,0 +1,7 @@
+class RupeetoDollar{
+public static void main(String args[]){
+int Indian_Currency=100;
+double Dollar=Indian_Currency/70;
+System.out.println(Dollar);
+}
+}
